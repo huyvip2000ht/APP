a46f075f2f4c48675aea1b3d86fa1323f4837f3d
Nguyen Tiep <nguyenthaitiep206@gmail.com>
2020-04-04 00:05:53 +0700

    Setep basic frames of App

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,1 +1,9 @@
 //to do
+var express = require('express')
+var router = express.Router();
+
+router.get('/create', function(request, response) {
+    response.render('users/create');
+})
+
+module.exports = router;
