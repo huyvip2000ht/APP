c3a7b836a235536f21319bdc406f1fd9474b046b
Nguyen Tiep <nguyenthaitiep206@gmail.com>
2020-04-07 23:57:49 +0700

    upload Market Screen drag and drop coin

--- Database/database.js
+++ Database/database.js
@@ -3,4 +3,15 @@ const FileSync = require('lowdb/adapters/FileSync')
 
 const adapter = new FileSync('./Database/db.json')
 const db = low(adapter)
+db.defaults({
+    user: [],
+
+    lesson: [
+        { id: "1", question: [1, 2, 8, 5, 6], product: "cream" },
+        { id: "2", question: [1, 2, 8, 5, 6], product: "cream" },
+        { id: "3", question: [1, 2, 8, 5, 6], product: "cream" },
+    ]
+
+
+}).write();
 module.exports = db;
